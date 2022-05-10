4668c43423d7c0cbe49da8d2338ce2c94ee95021
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2022-05-10 02:05:24 -0300

    ARCH-PC

--- files/.config/task/hooks/shift-recurrence/pirate_add_shift_recurrence.py
+++ files/.config/task/hooks/shift-recurrence/pirate_add_shift_recurrence.py
@@ -17,14 +17,14 @@ def is_new_local_recurrence_child_task(task):
     if (task['modified'] - task['entry']).total_seconds() < 0:
         return True
 
-tw = TaskWarrior(data_location=os.path.dirname(os.path.dirname(sys.argv[0])))
+tw = TaskWarrior(data_location=os.getenv('HOME')+"/.local/share/task")
 tw.overrides.update(dict(recurrence="no", hooks="no"))
 
 def hook_shift_recurrence(task):
     if is_new_local_recurrence_child_task(task):
         parent = tw.tasks.get(uuid=task['parent']['uuid'])
         parent_due_shift = task['due'] - parent['due']
-        parent['until'] += parent_due_shift
         for attr in time_attributes:
             if parent[attr]:
                 task[attr] = parent[attr] + parent_due_shift
+        # parent['until'] += parent_due_shift
