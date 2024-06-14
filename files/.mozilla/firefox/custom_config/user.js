073c1a7165361ba78553409232b2669b018e854d
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2024-06-13 22:37:39 -0300

    ARCH-PC

--- files/.mozilla/firefox/custom_config/user.js
+++ files/.mozilla/firefox/custom_config/user.js
@@ -1,5 +1,22 @@
-user_pref("browser.aboutConfig.showWarning", false);
+////////////
+// Native //
+////////////
+
+// userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("extensions.pocket.enabled", false);
+
+// no warning on about:config
+user_pref("browser.aboutConfig.showWarning", false);
+
+// no waring on fullscreen
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("full-screen-api.transition.timeout", 0);
+
+////////////
+// Onebar //
+////////////
+user_pref("onebar.disable-https-truncate", true);
+user_pref("onebar.hide-all-URLbar-icons", true);
+user_pref("onebar.disable-centering-of-URLbar", false);
+user_pref("onebar.disable-single-tab", true);
+user_pref("onebar.hide-all-tabs-button", true);
