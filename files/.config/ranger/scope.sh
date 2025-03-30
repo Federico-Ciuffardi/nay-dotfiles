dd8bd96c12d0dd35477d2f4f34c5ede63988ca15
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2025-03-30 13:06:39 -0300

    archlinux

--- files/.config/ranger/scope.sh
+++ files/.config/ranger/scope.sh
@@ -302,7 +302,7 @@ handle_mime() {
             exit 1;;
 
         ## Text
-        text/* | */xml)
+        text/* | */xml | application/*)
             ## Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
